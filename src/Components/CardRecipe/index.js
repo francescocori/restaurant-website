3cd0aa4badee15030061770899a517c5bc54dd62
franncesco <francesco.coriolano@gmail.com>
2022-06-02 21:14:35 +0200

    added style to popular recipes card

--- src/Components/CardRecipe/index.js
+++ src/Components/CardRecipe/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 const CardRecipe = ({ image, text, title }) => {
   return (
-    <div className="card-recipe">
-      <div className="card-recipe-img">
+    <div className="card-recipe-container">
+      <div className="img-container">
         <img src={image} alt="food" className="img" />
+        <div className="servings">
+          <div>
+            <span>4</span> <br /> Serving
+          </div>
+        </div>
+        <div className="arrow-back">
+          <FontAwesomeIcon icon={faArrowRight} />
+        </div>
       </div>
-      <div className="card-recipe-text">
-        <div className="g">{title}</div>
-        <p className="t">{text}</p>
+      <div className="text-container">
+        <span>difficulty</span>
+        <div className="item-name">{title}</div>
+        <p className="item-description">{text}</p>
       </div>
     </div>
   );
--- src/Data/PopularRecipesData.js
+++ src/Data/PopularRecipesData.js
@@ -13,44 +13,44 @@ const popularRecipes = [
     id: "a1",
     image: toast,
     title: "CHARRED BROCCOLI  VEGGIEROMESCO TOAST",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime non molestiae esse quod fuga, illum minima amet consequuntur at impedit eveniet. Aut? ",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime   ",
   },
 
   {
     id: "a2",
     image: pineapple,
     title: "PINEAPPLE FRUIT SALADWITH FRESH CREAM",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime non molestiae esse quod fuga, illum minima amet consequuntur at impedit eveniet. Aut? ",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime ",
   },
   {
     id: "a3",
     image: mushroomPasta,
     title: "CHEESE, SPINACH MUSHROOM STUFFED CHI...",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime non molestiae esse quod fuga, illum minima amet consequuntur at impedit eveniet. Aut? ",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime ",
   },
   {
     id: "a4",
     image: fetaSalad,
-    title: "FETA AND PEACHCOUSCOUS",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime non molestiae esse quod fuga, illum minima amet consequuntur at impedit eveniet. Aut?",
+    title: "FETA AND PEACHCOUSCOUS SALAD",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime ",
   },
   {
     id: "a5",
     image: fishAndChips,
     title: "fishAndChips",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime non molestiae esse quod fuga, illum minima amet consequuntur at impedit eveniet. Aut?",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime animi maxime ",
   },
   {
     id: "a6",
     image: tiramisu,
     title: "Super-Tiramisu",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime non molestiae esse quod fuga, illum minima amet consequuntur at impedit eveniet. Aut?",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime animi maxime ",
   },
   {
     id: "a4",
     image: pizza,
     title: "Italian_Pizza",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime non molestiae esse quod fuga, illum minima amet consequuntur at impedit eveniet. Aut?",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure nesciunt ducimus quisquam exercitationem repellendus recusandae animi maxime animi maxime ",
   },
 ];
 export default popularRecipes;
