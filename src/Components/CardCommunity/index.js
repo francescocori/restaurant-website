c9d6190bb9b6c405389abc1eea9238170edd0200
franncesco <francesco.coriolano@gmail.com>
2022-06-04 12:02:55 +0200

    added animation on community section

--- src/Components/CardCommunity/index.js
+++ src/Components/CardCommunity/index.js
@@ -1,32 +1,39 @@
 import React, { useEffect, useState } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import "./style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMessage, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 const CardCommunity = ({ id, title, text, image, likes, comments }) => {
-  const [offsetY, setOffsetY] = useState(0);
-  const [show, setShow] = useState(false);
+  // const [offsetY, setOffsetY] = useState(0);
+  // const [show, setShow] = useState(false);
 
-  //calculate scroll
-  const handleScroll = () => {
-    setOffsetY(window.pageYOffset);
-    if (offsetY > 200) {
-      setShow(true);
-      console.log("over 200");
-    } else if (offsetY < 200) {
-      setShow(false);
-    }
-  };
+  // //calculate scroll
+  // const handleScroll = () => {
+  //   setOffsetY(window.pageYOffset);
+  //   if (offsetY > 200) {
+  //     setShow(true);
+  //     console.log("over 200");
+  //   } else if (offsetY < 200) {
+  //     setShow(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", handleScroll);
+  //   console.log("show", show);
+
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, [show]);
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    console.log("show", show);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [show]);
+    Aos.init({ duration: 2000 });
+  }, []);
 
   return (
-    <div className={`card-community ${show ? "show" : ""}`}>
+    // <div className={`card-community ${show ? "show" : ""}`}>
+    <div className="card-community" data-aos="flip-left">
       <div className="card-img">
         <img src={image} alt="food" className="img" />
 
