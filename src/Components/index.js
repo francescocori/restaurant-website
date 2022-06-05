056fc61da66ab1f1c1507bf13a6119deec78b289
franncesco <francesco.coriolano@gmail.com>
2022-06-05 17:51:49 +0200

    ready to be deploy

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,7 +2,6 @@ export { default as NavBar } from "./NavBar";
 export { default as CardCommunity } from "./CardCommunity";
 export { default as CardReceipe } from "./CardRecipe";
 export { default as ContactUs } from "./ContactUs";
-export { default as SectionIndicator } from "./SectionIndicator";
 export { default as Footer } from "./Footer";
 export { default as MenuItem } from "./MenuItem";
 export { default as MenuColumn } from "./MenuColumn";
